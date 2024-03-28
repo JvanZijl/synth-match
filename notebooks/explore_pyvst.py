8d679e9b24969963da66a29147fde871dd063563
martijndejong <martijndejong@users.noreply.github.com>
2024-03-28 02:07:12 +0100

    Just stashing commits of trying out pyvst, but this branch can be deleted.

---
+++ notebooks/explore_pyvst.py
@@ -0,0 +1,38 @@
+import argparse
+from pyvst import SimpleHost
+
+
+def _print_params(vst, max_params=10):
+    """Prints the parameters of a VST with its current value."""
+    for i in range(min(vst.num_params, max_params)):
+        print('{}: {}'.format(
+            vst.get_param_name(i),
+            vst.get_param_value(i),
+        ))
+
+
+def main(vst_filename):
+    host = SimpleHost(vst_filename, sample_rate=48000.)
+    _print_params(host.vst)
+
+    sound = host.play_note(note=64, note_duration=1.)
+    print(sound)
+    print(sound.shape)
+
+    host.vst.set_param_value(index=0, value=1.)
+    host.vst.set_param_value(index=1, value=0.5)
+
+    _print_params(host.vst)
+
+    sound = host.play_note(note=64, note_duration=1.)
+    print(sound)
+    print(sound.shape)
+
+
+if __name__ == '__main__':
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('vst', help='path to .so file')
+    # args = parser.parse_args()
+
+    # main(args.vst)
+    main(vst_filename='C:/Program Files/VstPlugins/helm64.dll')
